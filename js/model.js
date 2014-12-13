9931f65c0a43bfb25c54dcddff21e2e8eb32c6a5
HazCod <hazcod@outlook.be>
2014-12-13 19:00:06 +0100

    Initial offline caching EXPERIMENTAL

--- js/model.js
+++ js/model.js
@@ -1,6 +1,6 @@
 var Model = function () {
 
-	this.getData = function(input) {
+	this.getDataOnline = function(input) {
 	// Get data and transform
 		result = false;
 		$.ajax({
@@ -34,9 +34,54 @@ var Model = function () {
 		return result;
 	},
 
+	//Requirement: questions must be fetched at least once on first boot
+	this.getData = function (input){
+		var ttl = this.getLocal('TTL_' + input.trim());
+		log('TTL is ' + ttl + ' for ' + input);
+		var now = new Date().format("yyMMdd");
+		if (!ttl || (ttl < now){
+			log('TTL expired or not existant. Fetching online..');
+			//if it doesnt exist cached or TTL is more than a day old
+			var result = this.getDataOnline(input);
+			if (result){
+				// Fill our local database
+				this.putLocal(input.trim(), result);
+				// Adjust/add the TTL
+				this.putLocal('TTL_' + input.trim(), now);
+				log('Putting result in cache..');
+				return result;
+			} else {
+				if (ttl){
+					log('No network, but we have a cached version.');
+					//Online doesnt work, but we have a local copy!
+					return this.getLocal(input.trim());
+				} else {
+					log('No network and no cached version.. Returning false..');
+					//Online doesnt work and we dont have a local copy..
+					return false;
+				}
+			}
+		} else {
+			log('TTL valid, opening from cache..');
+			//Safe to use our cached copy
+			return this.getLocal(input.trim());
+		}
+	},
+
 	this.getQuestions = function(catid) {
 		//GET secured/category/{id}(/random)
-		return this.getData(this.categoryURL + catid);
+		var cached = this.getLocal('questions');
+		if (this.getLocal('TTL_questions') > )
+		if (!cached){
+			cached = this.getData(this.categoryURL + catid);
+			if (cached){
+				this.putLocal('questions', cached);
+				this.putLocal('TTL_questions', new Date().format("ddMMyy"))
+			}
+		} else {
+			return cached;
+		}
+		return 
 	},
 
 	this.getQuestion = function(questionid) {
@@ -74,17 +119,72 @@ var Model = function () {
 		this.userURL 	   = this.secURL + 'user/';
 	},
 
-	this.correct = function (questionid) {
-		//TODO
+	this.sendNumbers = function() {
+		var solved_arr = this.getLocal('solved');
+		var wrong_arr  = this.getLocal('wrong');
+		if (solved_arr){
+			//TODO: send
+		}
+		if (wrong_arr){
+			//TODO: send
+		}
 	}
 
+	this.correct = function (questionid) {
+		var solved_arr = this.getLocal('solved');
+		if (solved_arr){
+			solved_arr.put(questionid);
+			if (this.sendNumbers()){
+				this.removeLocal('solved');
+			} else {
+				this.putLocal('solved', solved_arr);
+			}
+		} else {
+			this.putLocal('solved', [ questionid ]);
+		}
+	},
+
 	this.incorrect = function (questionid) {
-		//TODO
-	}
+		var wrong = this.getLocal('wrong');
+		if (wrong){
+			wrong = wrong +1;
+			if (this.setNumbers()){
+				this.removeLocal('wrong');
+			} else {
+				this.putLocal('wrong', wrong);
+			}
+		} else {
+			this.putLocal('wrong', 1);
+		}
+	},
+
+	this.removeLocal = function (key) {
+		return $.jStorage.deleteKey(key);
+	},
+
+	this.getLocal = function (key) {
+		return $.jStorage.get(key, false);
+	},
+
+	this.putLocal = function (key, value) {
+		return $.jStorage.set(key, value);
+	},
 
 	this.initialize = function () {
 	// Model constructor
+		//all online URLs
 		this.setupURLs();
+
+		//do we need to work offline?
+		this.online = (navigator.onLine);
+		log('Network status: ' + this.online);
+
+		//is this our first run?
+		if (!getLocal('first')){
+			//redirect('we_need_internet');
+			//TODO: show page until internet connection is reached
+			//TODO: fetchFirstLevels(all_categories)
+		}
 	},
 
 	this.initialize();
