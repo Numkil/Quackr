35b668d381384e2cd2de8d3399260be83dedcae9
HazCod <hazcod@outlook.be>
2014-12-16 21:20:24 +0100

    Update ProfileView.js
    Make it a bit tidier. It's late and i'm tired, so no more big code changes for today :-(

--- js/views/ProfileView.js
+++ js/views/ProfileView.js
@@ -1,21 +1,8 @@
 var ProfileView = function (data) {
     //View for the overview screen
     //
-
-    this.initialize = function () {
-        // View constructor
-        if (app.loggedin == true){
-            var cats = app.model.getCategories();
-            var provider = app.userProfile.identities[0].provider;
-            var myRegex = /(.*)\-oauth2/;
-            var provider = myRegex.exec(provider)[1];
-            render('profile', {
-                name: app.userProfile.name,
-                picture: app.userProfile.picture,
-                provider: provider,
-                categories: cats,
-            }).done( function(){
-                cats.forEach(function(cat){
+    this.createProgressBar = function() {
+        cats.forEach(function(cat){
                     var progress = app.model.getCategoryAnswered(cat.id);
                     $.extend(cat, progress);
                     log(cat);
@@ -28,6 +15,23 @@ var ProfileView = function (data) {
                         label: '%'
                     });
                 });
+    },
+    
+    this.initialize = function () {
+        // View constructor
+        if (app.loggedin == true){
+            var cats = app.model.getCategories();
+            var provider = app.userProfile.identities[0].provider;
+            var myRegex = /(.*)\-oauth2/;
+            var provider = myRegex.exec(provider)[1];
+            
+            render('profile', {
+                name: app.userProfile.name,
+                picture: app.userProfile.picture,
+                provider: provider,
+                categories: cats,
+            }).done( function(){
+                this.createProgressBar();
             });
         } else {
             //Re-render and show login page with login filled in
