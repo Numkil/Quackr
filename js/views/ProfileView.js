423694976e85195383c09cb5324300c723653cd5
Numkil <kevin.jossart@hotmail.com>
2014-12-16 14:12:36 +0100

    First tries for a progressbar

--- js/views/ProfileView.js
+++ js/views/ProfileView.js
@@ -1,29 +1,48 @@
 var ProfileView = function (data) {
-//View for the overview screen
+    //View for the overview screen
+    //
 
-	this.initialize = function () {
-	// View constructor
-		if (app.loggedin == true){
-			//log(app.userProfile);
-			
-			//log('random question:');
-			//log(app.model.getRandomQuestion('2'));
-
+    this.initialize = function () {
+        // View constructor
+        if (app.loggedin == true){
+            var cats = app.model.getCategories();
             var provider = app.userProfile.identities[0].provider;
             var myRegex = /(.*)\-oauth2/;
             var provider = myRegex.exec(provider)[1];
             render('profile', {
-            	name: app.userProfile.name,
-            	picture: app.userProfile.picture,
-                provider: provider
+                name: app.userProfile.name,
+                picture: app.userProfile.picture,
+                provider: provider,
+                categories: cats,
             });
-		} else {
-			//Re-render and show login page with login filled in
-			//TODO: Show error message
-			log('Not logged in for profile!');
-			redirect('#login');
-		}
-	}	
+            cats.forEach(function(cat){
+                var progress = app.model.getCategoryAnswered(cat.id);
+                $.extend(cat, progress);
+                var progressbar = (cat.sizeFinished / cat.sizeQuestions)*100;
+                $.getScript('js/progressbar.js').done(function(){
+                    //create the progressbar
+                    jQMProgressBar('pb_'+cat.id)
+                        .setOuterTheme('b')
+                        .setInnerTheme('e')
+                        .isMini(true)
+                        .setMax(progressbar)
+                        .setStartFrom(0)
+                        .setInterval(10)
+                        .showCounter(true)
+                        .build()
+                        .run();
+                });
+                //$("#pb_"+cat.id).progressbar({
+                    //value: progressbar,
+                //});
+            });
+        } else {
+            //Re-render and show login page with login filled in
+            //TODO: Show error message
+            log('Not logged in for profile!');
+            redirect('#login');
+        }
+    }
 
-	this.initialize();
+    this.initialize();
 }
