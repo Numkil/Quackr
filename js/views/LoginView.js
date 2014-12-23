78922a1722cb572519179c753cd49767ce2f619f
Numkil <kevin.jossart@hotmail.com>
2014-12-23 09:59:50 +0100

    clear not working test

--- js/views/LoginView.js
+++ js/views/LoginView.js
@@ -50,24 +50,6 @@ var LoginView = function (data) {
                     });
                 });
                 render('login', {}).done(function(){
-                    navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
-
-
-                    // onSuccess: Get a snapshot of the current acceleration
-                    //
-                    function onSuccess(acceleration) {
-                        alert('Acceleration X: ' + acceleration.x + '\n' +
-                                'Acceleration Y: ' + acceleration.y + '\n' +
-                                'Acceleration Z: ' + acceleration.z + '\n' +
-                                'Timestamp: '      + acceleration.timestamp + '\n');
-                    }
-
-                    // onError: Failed to get the acceleration
-                    //
-                    function onError() {
-                        alert('onError!');
-                    }
-
                 });
             } else if (loggedin && !first){
                 redirect('overview');
