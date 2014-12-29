3ddf175151eb4ba284e66b1a940a70b5ef0fbac8
HazCod <hazcod@outlook.be>
2014-12-29 11:38:11 +0100

    Better check for accelerometer

--- js/shake.js
+++ js/shake.js
@@ -7,7 +7,7 @@ var shake = (function () {
 	
 	// Start watching the accelerometer for a shake gesture
 	shake.startWatch = function (onShake) {
-		if (navigator.accelerometer){
+		if (typeof navigator.accelerometer !== 'undefined') {
 			if (onShake) {
 				shakeCallBack = onShake;
 			}
